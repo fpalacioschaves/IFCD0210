6c337ee86a9c773ac280c4ccf52db8240b0027d6
fpalacioschaves <fpalacioschaves@gmail.com>
2022-03-09 14:22:33 +0100

    nuevos ejemplos
    nuevos ejemplos de PHP

---
+++ UF1844/ejemplos_php/constantes.php
@@ -0,0 +1,13 @@
+<?php
+
+define("PI", 3.14);
+print "<p>El valor de pi es " . PI . "</p>\n";
+
+define("AUTOR", "Bartolomé Sintes Marco");
+print "<p>Autor: " . AUTOR . "</p>\n";
+
+
+const AUTOR2 = "Bartolomé Sintes Marco";
+print "<p>Autor: " . AUTOR2 . "</p>\n";
+
+?>
---
+++ UF1844/ejemplos_php/while.php
@@ -0,0 +1,12 @@
+<?php
+$max = 0;
+	do
+	{
+		echo $max;
+
+        $max++;
+
+		// process the line content
+
+		} while($max < 10);
+?>
