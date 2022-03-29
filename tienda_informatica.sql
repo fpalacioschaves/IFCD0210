90424c1e11c24e4bc6204bc008a7220081ded513
fpalacioschaves <fpalacioschaves@gmail.com>
2022-03-29 13:51:57 +0200

    tienda informática

---
+++ tienda_informatica.sql
@@ -0,0 +1,126 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 29-03-2022 a las 13:50:56
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 8.1.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `tienda_informatica`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `fabricante`
+--
+
+CREATE TABLE `fabricante` (
+  `codigo` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `fabricante`
+--
+
+INSERT INTO `fabricante` (`codigo`, `nombre`) VALUES
+(1, 'Asus'),
+(2, 'Lenovo'),
+(3, 'Hewlett-Packard'),
+(4, 'Samsung'),
+(5, 'Seagate'),
+(6, 'Crucial'),
+(7, 'Gigabyte'),
+(8, 'Huawei'),
+(9, 'Xiaomi');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `producto`
+--
+
+CREATE TABLE `producto` (
+  `codigo` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `precio` double NOT NULL,
+  `codigo_fabricante` int(10) UNSIGNED NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `producto`
+--
+
+INSERT INTO `producto` (`codigo`, `nombre`, `precio`, `codigo_fabricante`) VALUES
+(1, 'Disco duro SATA3 1TB', 86.99, 5),
+(2, 'Memoria RAM DDR4 8GB', 120, 6),
+(3, 'Disco SSD 1 TB', 150.99, 4),
+(4, 'GeForce GTX 1050Ti', 185, 7),
+(5, 'GeForce GTX 1080 Xtreme', 755, 6),
+(6, 'Monitor 24 LED Full HD', 202, 1),
+(7, 'Monitor 27 LED Full HD', 245.99, 1),
+(8, 'Portátil Yoga 520', 559, 2),
+(9, 'Portátil Ideapd 320', 444, 2),
+(10, 'Impresora HP Deskjet 3720', 59.99, 3),
+(11, 'Impresora HP Laserjet Pro M26nw', 180, 3);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `fabricante`
+--
+ALTER TABLE `fabricante`
+  ADD PRIMARY KEY (`codigo`);
+
+--
+-- Indices de la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD PRIMARY KEY (`codigo`),
+  ADD KEY `codigo_fabricante` (`codigo_fabricante`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `fabricante`
+--
+ALTER TABLE `fabricante`
+  MODIFY `codigo` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT de la tabla `producto`
+--
+ALTER TABLE `producto`
+  MODIFY `codigo` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `producto`
+--
+ALTER TABLE `producto`
+  ADD CONSTRAINT `producto_ibfk_1` FOREIGN KEY (`codigo_fabricante`) REFERENCES `fabricante` (`codigo`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
