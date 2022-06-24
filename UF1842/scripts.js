904d8e68a5455ec2d5ef3c6e350b44d7d91060e4
Francisco Palacios Chaves <fpalacioschaves@gmail.com>
2022-06-24 14:05:51 +0200

    Add files via upload

---
+++ UF1842/scripts.js
@@ -0,0 +1,14 @@
+document.getElementById("ver_mas").addEventListener("click", mostrar_todo)
+
+function mostrar_todo(){
+    visible = document.getElementById("resumen")
+
+    oculto = document.getElementById("total")
+
+    visible.style.display = "none"
+
+    oculto.style.display = "block"
+}
+
+//////////////////////////////////////////////////////////////////
+
