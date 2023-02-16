b4bd870ec3c079606d554bf50fe31dcad80f0ef3
Francisco Palacios Chaves <fpalacioschaves@gmail.com>
2023-02-16 11:56:26 +0100

    Reorganizar contenidos

--- postem-ipsum/admin/views/json_export.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Fco Palacios Chaves
- * Date: 21/11/2018
- * Time: 15:22
- */
--- postem-ipsum/uninstall.php
+++
@@ -1,9 +0,0 @@
-<?php
-// More info : http://codex.wordpress.org/Function_Reference/register_uninstall_hook
-// If uninstall not called from WordPress, then exit.
-if ( ! defined ( 'WP_UNINSTALL_PLUGIN' ) )
-{
-	exit;
-
-}
-
